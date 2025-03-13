2223b6854381adea2fe5c21f827d514197569d32
husain <hsingapurwala@gmail.com>
2025-03-13 22:28:52 +0530

    making text lowercase and added some text info

--- src/components/TextForms.js
+++ src/components/TextForms.js
@@ -1,28 +1,39 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 
 export default function TextForms(props) {
-    const handleUpClick = () =>{
-        console.log("Uppercase was clicked" + text);
+    const handleUpClick = () => {
         let newText = text.toUpperCase();
         setText(newText);
     };
 
-    const handlemyBoxTextChange = (event) =>{
-        console.log("Onchange");
+    const handleLcClick = () => {
+        let newText = text.toLowerCase();
+        setText(newText);
+    };
+
+    const handlemyBoxTextChange = (event) => {
         setText(event.target.value);
     };
 
-const [text, setText] = useState("Enter your text here");
-
-
+    const [text, setText] = useState("");
 
     return (
-        <div>
-            <h1>{props.heading}</h1>
-            <div className="mb-3 mt-5">
-                <textarea value={text} onChange={handlemyBoxTextChange} className="form-control mb-3" id="myBox" rows="8"></textarea>
-                <button className='btn btn-primary' onClick={handleUpClick} >Convert To Uppercase</button>
+        <>
+            <div className='container'>
+                <h1>{props.heading}</h1>
+                <div className="mb-3 mt-5">
+                    <textarea placeholder='Enter Text Here...' value={text} onChange={handlemyBoxTextChange} className="form-control mb-3" id="myBox" rows="8"></textarea>
+                    <button className='btn btn-primary mx-2' onClick={handleUpClick} >Convert To Uppercase</button>
+                    <button className='btn btn-info' onClick={handleLcClick} >Convert To Lowercase</button>
+                </div>
             </div>
-        </div>
+            <div className='container my-4'>
+                <h1>Your Text Summary</h1>
+                <p>{text.trim().split(/\s+/).filter(word => word.length > 0).length} words and {text.length} characters</p>
+                <p>{text.trim().split(/\s+/).filter(word => word.length > 0).length * 0.008} Minutes to read</p>
+                <h3>Preview</h3>
+                <p>{text}</p>
+            </div>
+        </>
     )
 }
